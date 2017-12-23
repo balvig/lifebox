888a496ecb861096dc79a82ea5bde02ee6207a64
Jens Balvig <jens@balvig.com>
2017-12-23 18:12:00 +0900

    parse results from API

--- lifebox.ino
+++ lifebox.ino
@@ -1,28 +1,31 @@
 // Libs
 #include "Led.h"
 #include <WiFi.h>
+#include <HTTPClient.h>
 
 // Variables
 RBD::Timer timer;
 Lifebox::Led leds[] = { 12, 14, 27, 26, 25, 33, 32 };
 Lifebox::Led debugLed(2); // Built-in LED
 
-
 // Constants
+#define REFRESH_INTERVAL 5000
 #define LED_COUNT sizeof(leds) / sizeof(Lifebox::Led)
+
+#define API_ENDPOINT "http://1b524aa3.ngrok.io/lifebox"
 
 // Main
 void setup() {
   delay(10);
   Serial.begin(115200);
-  timer.setTimeout(5000);
+  timer.setTimeout(REFRESH_INTERVAL);
   timer.restart();
   connectToWifi();
   updateState();
 }
 
 void loop() {
-  if(timer.onRestart()) {
+  if (timer.onRestart()) {
     updateState();
   }
   updateLights();
@@ -37,21 +40,57 @@ void updateLights() {
 }
 
 void updateState() {
-  if(WiFi.status() != WL_CONNECTED) {
+  if (WiFi.status() != WL_CONNECTED) {
     debugLed.blink(300, 300);
   }
   else {
-    debugLed.on();
-    int results[] = { random(3), random(3) , random(3), random(2), random(2), random(2), random(2) };
+    String results = fetchResults();
+    char charBuf[50];
+    results.toCharArray(charBuf, 50);
+    char *token = strtok(charBuf, ",");
+    int i = 0;
+    
+    while (token) {
+      String tokenString = token;
+      int state = tokenString.toInt();
+      Serial.print(i);
+      Serial.print(": ");
+      Serial.print(state);
+      Serial.println("");
 
-    for (int i = 0; i < LED_COUNT; i++) {
-      int state = results[i];
       leds[i].setState(state);
+      
+      token = strtok(NULL, ",");
+      i++;
     }
+
+    debugLed.on();
   }
 }
 
 void connectToWifi() {
-  WiFi.begin(".........", ".........");
+  WiFi.begin(WIFI_NAME, WIFI_PASS);
 }
 
+String fetchResults() {
+  HTTPClient http;
+  http.begin(API_ENDPOINT);
+
+  int httpCode = http.GET();
+
+  if (httpCode > 0) {
+    Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+
+    if (httpCode == HTTP_CODE_OK) {
+      String payload = http.getString();
+      http.end();
+      Serial.println(payload);
+      return payload;
+    }
+  }
+  else {
+    http.end();
+    return "";
+    //USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+  }
+}
