96d0d710b9109cc89bd98ce1af4e9c54031fa121
Jens Balvig <jens@balvig.com>
2018-01-05 17:16:17 +0900

    set to "off" position on failure

--- recyclebox/recyclebox.ino
+++ recyclebox/recyclebox.ino
@@ -7,6 +7,7 @@
 const int SLEEPING_INTERVAL = 3600000; // 60 minutes
 //const int SLEEPING_INTERVAL = 10000; // 10 sec
 const size_t JSON_BUFFER = JSON_OBJECT_SIZE(1) + 20; // http://arduinojson.org/assistant/
+const int INIT_HAND_POSITION = 180;
 const String API_ENDPOINT = "http://lifeboxes.herokuapp.com/recycle";
 
 // Variables
@@ -18,6 +19,7 @@ Servo hand;
 void setup() {
   Serial.begin(115200);
   hand.attach(16);
+  hand.write(INIT_HAND_POSITION);
   net.connect(); // For some reason Wemos D1 won't reconnect after disconnect so keeping connected
 }
 
@@ -28,6 +30,6 @@ void loop() {
 
 void syncWithApi() {
   JsonObject& root = api.fetchJson();
-  int degrees = root["degrees"];
+  int degrees = root["degrees"] | INIT_HAND_POSITION;
   hand.write(degrees);
 }
