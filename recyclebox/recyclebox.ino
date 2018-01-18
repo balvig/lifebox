1dd77e3b0e5bb2a091000169721046902445f1c9
Jens Balvig <jens@balvig.com>
2018-01-18 16:57:11 +0900

    add proper deep sleep

--- recyclebox/recyclebox.ino
+++ recyclebox/recyclebox.ino
@@ -4,17 +4,19 @@
 #include "Servo.h"
 
 // Configuration
-const int SLEEPING_INTERVAL = 3600000; // 60 minutes
-//const int SLEEPING_INTERVAL = 5000; // 5 sec
+const int SLEEPING_INTERVAL = 60 * 60; // 60 minutes
+//const int SLEEPING_INTERVAL = 5; // 5 sec
 const size_t JSON_BUFFER = JSON_OBJECT_SIZE(1) + 20; // http://arduinojson.org/assistant/
 const String API_ENDPOINT = "http://lifeboxes.herokuapp.com/recycle";
 //const String API_ENDPOINT = "http://a0e839bc.ngrok.io/recycle";
 const int INIT_HAND_POSITION = 180;
+const int HAND_PIN = 5;
 
 // Variables
 Lifeboxes::Net net;
 Lifeboxes::Api api(API_ENDPOINT, JSON_BUFFER);
 Servo hand;
+
 
 // Main
 void setup() {
@@ -24,14 +26,18 @@ void setup() {
 
 void loop() {
   syncWithApi();
-  delay(SLEEPING_INTERVAL);
+  goToSleep(); // D0-RST on Wemos, remove when uploading...
 }
 
 void syncWithApi() {
   JsonObject& root = api.fetchJson();
   int degrees = root["degrees"] | INIT_HAND_POSITION;
-  hand.attach(16);
+  hand.attach(HAND_PIN);
   hand.write(degrees);
   delay(500);
   hand.detach();
+}
+
+void goToSleep() {
+  ESP.deepSleep(SLEEPING_INTERVAL * 1000000);
 }
