8c19e85634091a6973b7612f4dabce362626c7a4
Jens Balvig <jens@balvig.com>
2018-02-11 13:39:57 +0900

    kitchenbox stuff

--- kitchenbox/kitchenbox.ino
+++ kitchenbox/kitchenbox.ino
@@ -1,13 +1,13 @@
 // E-paper display
 #include <GxEPD.h>
-#include <GxGDE0213B1/GxGDE0213B1.cpp>      // 2.13" b/w
+//#include <GxGDE0213B1/GxGDE0213B1.cpp>      // 2.13" b/w
+#include <GxGDEW029Z10/GxGDEW029Z10.cpp>    // 2.9" b/w/r
 #include <GxIO/GxIO_SPI/GxIO_SPI.cpp>
 #include <GxIO/GxIO.cpp>
 
 // Libs
 #include "Api.h"
 #include "Net.h"
-#include "Paper.h"
 
 // Constants
 const size_t JSON_BUFFER = JSON_OBJECT_SIZE(1) + 310; // http://arduinojson.org/assistant/
@@ -17,17 +17,31 @@ const String API_ENDPOINT = "http://lifeboxes.herokuapp.com/kitchen";
 //GxIO_Class io(SPI, SS, 0, 2);
 //GxEPD_Class display(io);
 
-GxIO_Class io(SPI, SS, 4, 2); // arbitrary selection of D3(=0), D4(=2), selected for default of GxEPD_Class
-GxEPD_Class display(io, 2, 5); // default selection of D4(=2), D2(=4)
+//GxIO_Class io(SPI, SS, 4, 2); // arbitrary selection of D3(=0), D4(=2), selected for default of GxEPD_Class
+//GxEPD_Class display(io, 2, 5); // default selection of D4(=2), D2(=4)
+
+GxIO_Class io(SPI, SS, 0, 2); // arbitrary selection of D3(=0), D4(=2), selected for default of GxEPD_Class
+GxEPD_Class display(io); // default selection of D4(=2), D2(=4)
+
+
+//#include <Fonts/FreeSerif9pt7b.h>
+//const GFXfont* FONT = &FreeSerif9pt7b;
 
 
 Lifeboxes::Net net;
 Lifeboxes::Api api(API_ENDPOINT, JSON_BUFFER);
-Lifeboxes::Paper paper(&display);
+//Lifeboxes::Paper paper(&display);
 
 void setup() {
   Serial.begin(115200);
-  paper.renderText("\n\n\n\n\n\n\n\n\n\n\n\n  Loading recipe...", 1, false);
+
+  display.init();
+  display.setRotation(1);
+    //_display->setFont(FONT);
+  display.setTextColor(GxEPD_BLACK);
+  delay(1000);
+  
+  renderText("\n\n\n\n\n\n\n\n\n\n\n\n  Loading recipe...");
   net.connect();
   loadRecipe();
   net.disconnect();
@@ -40,5 +54,22 @@ void loop() {
 void loadRecipe() {
   JsonObject& root = api.fetchJson();
   const String recipe = root["recipe"];
-  paper.renderText(recipe);
+  renderText(recipe);
+}
+
+void renderText(const String text) {
+  int page = 1;
+  int offset = (page - 1) * GxEPD_HEIGHT;
+  boolean fullRefresh = true;
+  
+  display.setCursor(0, -offset);
+  display.fillScreen(GxEPD_WHITE);
+  display.println(text);
+
+  if (fullRefresh) {
+    display.update();
+  }
+  else {
+    display.updateWindow(0, 0, GxEPD_WIDTH, GxEPD_HEIGHT, false);
+  }   
 }
