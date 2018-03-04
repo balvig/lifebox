652ebb7ed15a52de5238ebac165d26da6a47c24e
Jens Balvig <jens@balvig.com>
2018-03-04 21:14:41 +0900

    add inspirebox

--- recyclebox/recyclebox.ino
+++ recyclebox/recyclebox.ino
@@ -4,13 +4,13 @@
 #include "Servo.h"
 
 // Configuration
-const int SLEEPING_INTERVAL = 60 * 60; // 60 minutes
+const int SLEEPING_INTERVAL = 60 * 60 * 24; // 60 minutes * 24
 //const int SLEEPING_INTERVAL = 5; // 5 sec
 const size_t JSON_BUFFER = JSON_OBJECT_SIZE(1) + 20; // http://arduinojson.org/assistant/
 const String API_ENDPOINT = "http://lifeboxes.herokuapp.com/recycle";
 //const String API_ENDPOINT = "http://a0e839bc.ngrok.io/recycle";
 const int INIT_HAND_POSITION = 180;
-const int HAND_PIN = 5;
+const int HAND_PIN = 2;
 
 // Variables
 Lifeboxes::Net net;
@@ -21,20 +21,21 @@ Servo hand;
 // Main
 void setup() {
   Serial.begin(115200);
+  syncWithApi();
+  goToSleep(); // D0-RST on Wemos, remove when uploading...
 }
 
 void loop() {
-  syncWithApi();
-  goToSleep(); // D0-RST on Wemos, remove when uploading...
 }
 
 void syncWithApi() {
   net.connect(); // For some reason Wemos D1 won't reconnect after disconnect so keeping connected
   JsonObject& root = api.fetchJson();
   int degrees = root["degrees"] | INIT_HAND_POSITION;
+  Serial.println(degrees);
   hand.attach(HAND_PIN);
   hand.write(degrees);
-  delay(500);
+  delay(1000);
   hand.detach();
 }
 
